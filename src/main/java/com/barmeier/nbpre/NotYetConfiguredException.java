6f42dc319778982ca474ef60470e420452df9497
barmeier <barmeier@copernicus>
2010-02-06 19:47:30 +0100

    Exception to thrwo if the Palm SDK was not configired correctly

---
+++ src/main/java/com/barmeier/nbpre/NotYetConfiguredException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.barmeier.nbpre;
+
+/**
+ *
+ * @author barmeier
+ */
+public class NotYetConfiguredException extends Exception {
+
+    public NotYetConfiguredException(String message) {
+        super (message);
+    }
+
+
+}
